e7c7474783efabf3421ebf989d7cdfe6c8fbb796
Michele Murphy <mmichelemurphy@gmail.com>
2014-05-18 12:05:45 -0700

    my initial code

--- week_4/5_pad_array/my_solution.rb
+++ week_4/5_pad_array/my_solution.rb
@@ -29,10 +29,32 @@
 # 2. Initial Solution
 
 
+	class Array                         # uses existing Ruby class  
+		def pad!(min_size, pad_value = nil)  #destructive (! = replace)
+  			if self.length >= min_size  # for existing array >= desired array size 
+  				return self				# use existing array
+  			else						# otherwise until size requirement
+	  			while self.length < min_size  # add to array with desired
+  					self.push(pad_value) 	# padding value
+  				end
+  				self					#return final padded array
+  			end	
+  		end
+
+
+		def pad(min_size, pad_value = nil) # non-destructive
+			self.clone.pad!(min_size, pad_value) #make shallow copy of array 
+			# (not its values) and objects referenced that can be modified 
+			# without modifying original array. Duplicate drops obj ref
+  		end	
+  	end
 
 
 # 3. Refactored Solution
-
+	# Am sure there is something that could be done to refactor but I don't
+	# know what that is. Maybe one day... Although I'm also realizing that
+	# I'm treating Refactor as something you do to code once it is 
+	# executing correctly. Perhaps I am approaching this wrong?
 
 
 # 4. Reflection 
