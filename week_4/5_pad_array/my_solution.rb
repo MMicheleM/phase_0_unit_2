53522bab6cc0377e7c45fd424d2205dd52214b85
Michele Murphy <mmichelemurphy@gmail.com>
2014-05-17 22:22:34 -0700

    my pseudocode

--- week_4/5_pad_array/my_solution.rb
+++ week_4/5_pad_array/my_solution.rb
@@ -4,14 +4,27 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
+=begin
+	
 # 1. Pseudocode
 
 # What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
+	Variety of methods to modify an array. Need to be be able to both
+	overwrite and not overwrite during array modification. Need to be
+	able to not pad an array under "X condition" as well as adding to
+	the end of an array, padding with a string, an object or with nil.
 
+# What is the output? (i.e. What should the code return?)
+	In most cases it should return a modified array. Exceptions would 
+	be when it's not meant to pad an array.
+
+# What are the steps needed to solve the problem?
+	Run test code to see initial error message. Tackle the test code 
+	one test at a time by creating method(s) to address the tests.
+
+=end
 
 # 2. Initial Solution
 
